0ad8a1d754491f9464657c48c1b86db136ce3408
Jihoon Lee <jihoonlee.in@gmail.com>
2014-06-26 08:06:50 +0900

    Imported upstream version '0.5.0' of 'upstream'

--- ar_track_alvar/nodes/IndividualMarkers.cpp
+++ ar_track_alvar/nodes/IndividualMarkers.cpp
@@ -39,13 +39,14 @@
 #include "ar_track_alvar/MarkerDetector.h"
 #include "ar_track_alvar/Shared.h"
 #include <cv_bridge/cv_bridge.h>
-#include <ar_track_alvar/AlvarMarker.h>
-#include <ar_track_alvar/AlvarMarkers.h>
+#include <ar_track_alvar_msgs/AlvarMarker.h>
+#include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <tf/transform_listener.h>
 #include <sensor_msgs/image_encodings.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <dynamic_reconfigure/server.h>
 #include <ar_track_alvar/ParamsConfig.h>
+#include <Eigen/StdVector>
 
 namespace gm=geometry_msgs;
 namespace ata=ar_track_alvar;
@@ -65,7 +66,7 @@ ros::Subscriber cloud_sub_;
 ros::Publisher arMarkerPub_;
 ros::Publisher rvizMarkerPub_;
 ros::Publisher rvizMarkerPub2_;
-ar_track_alvar::AlvarMarkers arPoseMarkers_;
+ar_track_alvar_msgs::AlvarMarkers arPoseMarkers_;
 visualization_msgs::Marker rvizMarker_;
 tf::TransformListener *tf_listener;
 tf::TransformBroadcaster *tf_broadcaster;
@@ -272,7 +273,7 @@ void GetMarkerPoses(IplImage *image, ARCloud &cloud) {
       printf("\n--------------------------\n\n");
       for (size_t i=0; i<marker_detector.markers->size(); i++)
      	{
-	  vector<cv::Point> pixels;
+	  vector<cv::Point, Eigen::aligned_allocator<cv::Point> > pixels;
 	  Marker *m = &((*marker_detector.markers)[i]);
 	  int id = m->GetId();
 	  cout << "******* ID: " << id << endl;
@@ -442,7 +443,7 @@ void getPointCloudCallback (const sensor_msgs::PointCloud2ConstPtr &msg)
 	  tf::Transform tagPoseOutput = CamToOutput * markerPose;
 
 	  //Create the pose marker messages
-	  ar_track_alvar::AlvarMarker ar_pose_marker;
+	  ar_track_alvar_msgs::AlvarMarker ar_pose_marker;
 	  tf::poseTFToMsg (tagPoseOutput, ar_pose_marker.pose.pose);
 	  ar_pose_marker.header.frame_id = output_frame;
 	  ar_pose_marker.header.stamp = image_msg->header.stamp;
@@ -508,7 +509,7 @@ int main(int argc, char *argv[])
   cam = new Camera(n, cam_info_topic);
   tf_listener = new tf::TransformListener(n);
   tf_broadcaster = new tf::TransformBroadcaster();
-  arMarkerPub_ = n.advertise < ar_track_alvar::AlvarMarkers > ("ar_pose_marker", 0);
+  arMarkerPub_ = n.advertise < ar_track_alvar_msgs::AlvarMarkers > ("ar_pose_marker", 0);
   rvizMarkerPub_ = n.advertise < visualization_msgs::Marker > ("visualization_marker", 0);
   rvizMarkerPub2_ = n.advertise < visualization_msgs::Marker > ("ARmarker_points", 0);
 	
